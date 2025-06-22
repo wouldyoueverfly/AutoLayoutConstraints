605e0e96e22dc78e5f7f2d178b3c1c07da4541bf
wouldyoueverfly <cpptypxe@ya.ru>
2025-06-22 20:48:24 +0300

    []

--- AutoLayoutConstraints/ViewController.swift
+++ AutoLayoutConstraints/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelCenterYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var labelTopConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonPushed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        let leftConstraint = button.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        leftConstraint.isActive = true
+        let rightConstraint = button.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        rightConstraint.isActive = true
+        let bottomConstraint = button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        bottomConstraint.isActive = true
+        
     }
 
+    @objc func buttonPushed(_ button: UIButton) {
+        if labelCenterYConstraint.isActive {
+            labelCenterYConstraint.isActive = false
+            labelTopConstraint.isActive = true
+        } else {
+            labelCenterYConstraint.isActive = true
+            labelTopConstraint.isActive = false
+        }
+        
+    }
 
 }
 
